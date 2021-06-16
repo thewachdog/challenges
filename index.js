7ef21ffe30043028d3912406770f184ff81fd66f
SowreRajan-dev <75199296+SowreRajan-dev@users.noreply.github.com>
2021-06-16 11:30:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,89 +7,94 @@ button.forEach((ele) => {
   });
 });
 
-let correctans=0;
+let correctans = 0;
 
-function ans1(btn){
-
-  if (document.getElementById('a').value=='apple'){
+function ans1(btn) {
+  if (document.getElementById("a").value == "apple") {
     document.getElementById(btn.id).disabled = true;
     alert("Button has been disabled.");
-    correctans+=1;
+    correctans += 1;
     console.log(correctans);
-    if (correctans==5){
-      document.getElementById('navigate').disabled = false;
-      alert('button was unlocked');
+    if (correctans == 5) {
+      document.getElementById("navigate").disabled = false;
+      alert("button was unlocked");
     }
-  }else{
-    alert('try again')
+  } else {
+    alert("try again");
   }
 }
 
-function ans2(btn){
-
-  if (document.getElementById('b').value=='apple'){
+function ans2(btn) {
+  if (document.getElementById("b").value == "apple") {
     document.getElementById("button2").disabled = true;
     alert("Button has been disabled.");
-    correctans+=1;
+    correctans += 1;
     console.log(correctans);
-    if (correctans==5){
-      document.getElementById('navigate').disabled = false;
-      alert('button was unlocked');
+    if (correctans == 5) {
+      document.getElementById("navigate").disabled = false;
+      alert("button was unlocked");
     }
-  }else{
-    alert('try again')
+  } else {
+    alert("try again");
   }
 }
 
-function ans3(btn){
-
-  if (document.getElementById('c').value=='apple'){
+function ans3(btn) {
+  if (document.getElementById("c").value == "apple") {
     document.getElementById(btn.id).disabled = true;
     alert("Button has been disabled.");
-    correctans+=1;
+    correctans += 1;
     console.log(correctans);
-    if (correctans==5){
-      document.getElementById('navigate').disabled = false;
-      alert('button was unlocked');
+    if (correctans == 5) {
+      document.getElementById("navigate").disabled = false;
+      alert("button was unlocked");
     }
-  }else{
-    alert('try again')
+  } else {
+    alert("try again");
   }
 }
 
-function ans4(btn){
-
-  if (document.getElementById('d').value=='apple'){
+function ans4(btn) {
+  if (document.getElementById("d").value == "apple") {
     document.getElementById(btn.id).disabled = true;
     alert("Button has been disabled.");
-    correctans+=1;
+    correctans += 1;
     console.log(correctans);
- 
-    if (correctans==5){
-      document.getElementById('navigate').disabled = false;
-      alert('button was unlocked');
+
+    if (correctans == 5) {
+      document.getElementById("navigate").disabled = false;
+      alert("button was unlocked");
     }
-  }else{
-    alert('try again')
+  } else {
+    alert("try again");
   }
 }
 
-function ans5(btn){
-
-  if (document.getElementById('e').value=='apple'){
-    document.getElementById(btn.id).disabled = true;
+function ans5(btn) {
+  if (document.getElementById("e").value == "apple") {
+    // document.getElementById(btn.id).disabled = true;
     alert("Button has been disabled.");
-    correctans+=1;
+    correctans += 1;
     console.log(correctans);
 
-    if (correctans===5){
-      document.getElementById('navigate').disabled = false;
-      alert('button was unlocked');
-}
-  }else{
-    alert('try again')
+    if (correctans === 5) {
+      document.getElementById("navigate").disabled = false;
+      alert("button was unlocked");
+    }
+  } else {
+    alert("try again");
   }
+  return correctans;
 }
 
-document.getElementById('navigate').disabled = true;
-alert("Button has been disabled.");
+document.getElementById("navigate").disabled = true;
+// alert("Button has been disabled.");
+
+// function clickHandler() {
+//   console.log("answer: ", ans5());
+//   if (ans5() === 5) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
